8baaedc0ca57535ea128732c25491642de159f04
Matthew McClure <mrm8924@mavs.uta.edu>
2023-10-19 17:13:44 -0500

    Added second mode for unlockable door that will reset progress if left alone

--- 4392_TeamC_Prototype/Scripts/LockedDoor.gd
+++ 4392_TeamC_Prototype/Scripts/LockedDoor.gd
@@ -1,28 +1,36 @@
 extends StaticBody2D
 
-
 onready var NC = get_node("/root/RootNode/NoiseController")
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
+export var resetProgress = false
 
 var lastNoiseTime = 0
 var noiseInterval = 400
 var noiseIntensity = 100
 
+var totalTimeToUnlock = 100
 var timeToUnlock = 100
+
+var lastInteractTime = 0
+var inactiveInterval = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	# If resetProgress, clear any progress made after small interval of no Interact() calls.
+	if resetProgress and timeToUnlock < totalTimeToUnlock:
+		if OS.get_ticks_msec() - lastInteractTime > inactiveInterval:
+			timeToUnlock = totalTimeToUnlock
 
+# Object set to not be instant, so this will be called repeatedly while interacting.
 func Interact():
 	print("%d percent remaining" % [timeToUnlock])
 	timeToUnlock -= 1
+	
+	lastInteractTime = OS.get_ticks_msec()
 	
 	if OS.get_ticks_msec() - lastNoiseTime > noiseInterval:
 		lastNoiseTime = OS.get_ticks_msec()
