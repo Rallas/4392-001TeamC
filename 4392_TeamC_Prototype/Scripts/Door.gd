113a70d3ea13c6b56bda178c53aa53e48bdbd5b6
Matthew McClure <mrm8924@mavs.uta.edu>
2023-10-26 16:51:14 -0500

    changed exit to reload level instead of kicking back to menu

--- 4392_TeamC_Prototype/Scripts/Door.gd
+++ 4392_TeamC_Prototype/Scripts/Door.gd
@@ -7,5 +7,5 @@ onready var PlayerInventory = get_node("/root/RootNode/Player/Inventory")
 # Object set to not be instant, so this will be called repeatedly while interacting.
 func Interact():
   if PlayerInventory.carryingKey:
-    var _ret = get_tree().change_scene("res://Scenes/StartMenu.tscn")
+    var _ret = get_tree().reload_current_scene()
 
