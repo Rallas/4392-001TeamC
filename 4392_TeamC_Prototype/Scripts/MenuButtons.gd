2bd2f3a8ccef1ee2a586f34f9d69e4681412c53c
Nch2792 <nch2792@mavs.uta.edu>
2023-10-29 13:24:43 -0500

    Updated Menu Fixes

--- 4392_TeamC_Prototype/Scripts/MenuButtons.gd
+++ 4392_TeamC_Prototype/Scripts/MenuButtons.gd
@@ -9,14 +9,18 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
   var playButton = $Start
+  var optionsButton = $Options
   var exitButton = $Quit
   playButton.connect("pressed", self, "_playButton_Pressed")
+  optionsButton.connect("pressed", self, "_optionsButton_Pressed")
   exitButton.connect("pressed", self, "_exitButton_Pressed")
-
 
 func _playButton_Pressed():
   #load the first scene
   var _ret = get_tree().change_scene("res://Scenes/DefaultScene.tscn")
+
+func _optionsButton_Pressed():
+    var _opt = get_tree().change_scene("res://Scenes/OptionsMenu.tscn")
 
 func _exitButton_Pressed():
   #exit out of application
