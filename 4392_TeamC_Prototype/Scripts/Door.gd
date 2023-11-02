78387f1a16f9b711bec91f44fb7f597d03ca3d71
Matthew McClure <mrm8924@mavs.uta.edu>
2023-11-02 14:35:58 -0500

    Restored door functionality, moved exit functionality to other script

--- 4392_TeamC_Prototype/Scripts/Door.gd
+++ 4392_TeamC_Prototype/Scripts/Door.gd
@@ -1,11 +1,27 @@
-extends Node
+extends StaticBody2D
 
+onready var NC = get_node("/root/RootNode/NoiseController")
+onready var ColliderNode = get_node("./CollisionShape2D")
+onready var SpriteNode = get_node("./Sprite")
 
-onready var PlayerInventory = get_node("/root/RootNode/Player/Inventory")
+var noiseIntensity = 100
+export var isClosed = true
 
+# Called when the node enters the scene tree for the first time.
+func _ready():
+  updateState()
+  pass
+
+func updateState():
+  if isClosed:
+      ColliderNode.disabled = false
+      SpriteNode.texture = preload("res://Sprites/Door_Closed.png")
+  else:
+      ColliderNode.disabled = true
+      SpriteNode.texture = preload("res://Sprites/Door_Open.png")
 
 # Object set to not be instant, so this will be called repeatedly while interacting.
 func Interact():
-  if PlayerInventory.carryingKey:
-    var _ret = get_tree().reload_current_scene()
-
+  isClosed = not isClosed
+  NC.CreateNoise(global_position, noiseIntensity)
+  updateState()
---
+++ 4392_TeamC_Prototype/Scripts/ExitDoor.gd
@@ -0,0 +1,11 @@
+extends Node
+
+
+onready var PlayerInventory = get_node("/root/RootNode/Player/Inventory")
+
+
+# Object set to not be instant, so this will be called repeatedly while interacting.
+func Interact():
+  if PlayerInventory.carryingKey:
+    var _ret = get_tree().reload_current_scene()
+
