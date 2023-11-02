1d5cf14e08880b67e8c58e8b2691fa4d21a70ac2
Matthew McClure <mrm8924@mavs.uta.edu>
2023-11-02 16:37:44 -0500

    Changed initial scene to main menu. Fix: Player health persists when entering next level.

--- 4392_TeamC_Prototype/Scripts/ExitDoor.gd
+++ 4392_TeamC_Prototype/Scripts/ExitDoor.gd
@@ -1,8 +1,15 @@
 extends Node
 
+onready var Player = get_node("%Player")
+
 # Object set to not be instant, so this will be called repeatedly while interacting.
 func Interact():
   if PlayerInventory.carryingKey:
+    # Save global values for next level.
     Global.CurrentLevel += 1
+    Global.PlayerHealthAtLevelStart = Player.currentHealth
+    
+    # Reload scene
+    # Once level generation done, this will generate a new (harder) level.
     var _ret = get_tree().reload_current_scene()
 
