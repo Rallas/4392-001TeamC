27b874441bc69a5e43a12c9e8285346a6831b9b7
Nch2792 <nch2792@mavs.uta.edu>
2023-11-09 16:16:08 -0600

    Added Volume Control

---
+++ 4392_TeamC_Prototype/Scenes/VolumeSlider.gd
@@ -0,0 +1,13 @@
+extends Control
+
+onready var bus_index = AudioServer.get_bus_index("Master")
+onready var music_index = AudioServer.get_bus_index("Music")
+onready var music = $Music
+
+# Called when the node enters the scene tree for the first time.  
+func ready():
+  music.play()
+
+func _on_MusicSlider_value_changed(value):
+    AudioServer.set_bus_volume_db(bus_index, linear2db(value))
+    AudioServer.set_bus_mute(bus_index, value < .05)
